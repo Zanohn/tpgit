6ba285a778b148c3bc3e256dd4e65ccdc264c636
Zanohn <51notmorgan51@gmail.com>
2013-12-18 17:36:36 +0100

    commenting ArithmeticMethods header file woo

--- TP1/ArithmeticMethods.h
+++ TP1/ArithmeticMethods.h
@@ -1,11 +1,11 @@
-bool IsOdd(int);
+bool IsOdd(int); // determines whether an integer is odd (in which case it returns true) or even (in which case it returns false)
 
-bool IsPrime(unsigned int);
+bool IsPrime(unsigned int); // determines whether an integer is a prime (returns true if it is the case, false otherwise)
 
-int Factorial(unsigned int);
+int Factorial(unsigned int); // returns the factorial of the given integer
 
-int FactorialNoRecursion(unsigned int);
+int FactorialNoRecursion(unsigned int); // same as Factorial, but calculated with no recursion
 
-int GreatestPrimeFactor(unsigned int);
+int GreatestPrimeFactor(unsigned int); // returns the greatest prime divider of given integer
 
-void Pythagorician(void);
+void Pythagorician(void); // no fucken clue
